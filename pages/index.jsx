0f80bd54063a655fc666b77f0eb4c104790532bb
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-11-10 14:07:41 +0900

    mui5導入

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,24 @@
+import Container from "@mui/material/Container";
 import { getDatabase } from "../util/notion";
 import Card from "../components/Card";
 const Home = ({ result }) => {
   return (
-    <div>
-      {result.map((page) => {
-        console.dir(page, { depth: null });
-        return (
-          <Card
-            title={page.properties.Post.title[0].plain_text}
-            link={`/${page.id}`}
-            tag="プログラミング"
-            date={page.created_time}
-            key={page.id}
-          />
-        );
-      })}
-    </div>
+      <Container maxWidth="sm">
+        <div>
+          {result.map((page) => {
+            console.dir(page, { depth: null });
+            return (
+                <Card
+                    title={page.properties.Post.title[0].plain_text}
+                    link={`/${page.id}`}
+                    tag="プログラミング"
+                    date={page.created_time}
+                    key={page.id}
+                />
+            );
+          })}
+        </div>
+      </Container>
   );
 };
 
